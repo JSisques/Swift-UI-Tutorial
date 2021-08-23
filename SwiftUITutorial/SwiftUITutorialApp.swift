934b7e858e8b17328d0e1029e20ab94aa67d02ee
Javi Plaza <javierplazasisques@gmail.com>
2021-08-23 18:22:25 +0200

    Root commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Javier Plaza Sisqués on 23/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
